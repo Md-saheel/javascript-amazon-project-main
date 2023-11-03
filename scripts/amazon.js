4e9a5104fd0d0cf5746ea4b0adbd78e257023ae9
md-saheel <mdsaheel6666@gmail.com>
2023-11-03 20:55:07 +0530

    make cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -50,9 +50,19 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary">Add to Cart</button>
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${
+            product.name
+          }">
+          Add to Cart
+          </button>
         </div>
  `;
 });
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+  button.addEventListener('click', () => {
+    console.log(button.dataset);
+  });
+});
