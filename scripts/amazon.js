78868118f9ef71ac15fe5ee616bcf29779db2899
md-saheel <mdsaheel6666@gmail.com>
2023-11-03 20:55:39 +0530

    make inter

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -50,8 +50,8 @@ products.forEach((product) => {
             Added
           </div>
 
-          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-name="${
-            product.name
+          <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id="${
+            product.id
           }">
           Add to Cart
           </button>
@@ -63,6 +63,21 @@ document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
 document.querySelectorAll('.js-add-to-cart').forEach((button) => {
   button.addEventListener('click', () => {
-    console.log(button.dataset);
+    const productId = button.dataset.productId;
+    let matchingItem;
+
+    cart.forEach((item) => {
+      if (productId === item.productId) {
+        matchingItem = item;
+      }
+    });
+
+    if (matchingItem) {
+      matchingItem.quantity += 1;
+    } else {
+      cart.push({ productId, quantity: 1 });
+    }
+
+    console.log(cart);
   });
 });
