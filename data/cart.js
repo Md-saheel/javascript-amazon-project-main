dfa2f00629ed392ffb50d70080b4000c2e64e305
md-saheel <mdsaheel6666@gmail.com>
2023-11-07 13:57:49 +0530

    localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,4 +1,4 @@
-export let cart = [
+export let cart = JSON.parse(localStorage.getItem('cart')) || [
   {
     productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
@@ -8,6 +8,10 @@ export let cart = [
     quantity: 1,
   },
 ];
+
+function saveToStorage() {
+  localStorage.setItem('cart', JSON.stringify(cart));
+}
 
 export function addToCart(productId) {
   let matchingItem;
@@ -32,6 +36,7 @@ export function addToCart(productId) {
       quantity,
     });
   }
+  saveToStorage();
 }
 
 export function removeFromCart(productId) {
@@ -44,4 +49,6 @@ export function removeFromCart(productId) {
   });
 
   cart = newCart;
+
+  saveToStorage();
 }
