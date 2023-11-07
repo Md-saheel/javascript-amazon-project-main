5b15e504e7d7a71ce6cf163bedc4403c50aff241
md-saheel <mdsaheel6666@gmail.com>
2023-11-07 13:48:05 +0530

    removed in cart

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -15,7 +15,8 @@ cart.forEach((cartItem) => {
     }
   });
   cartSummarytHTML += `
-<div class="cart-item-container">
+<div class="cart-item-container
+   js-cart-item-container-${matchingProduct.id}">
     <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
     <div class="cart-item-details-grid">
@@ -96,5 +97,11 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const { productId } = link.dataset;
     removeFromCart(productId);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+
+    container.remove();
   });
 });
