dcfb06e79146beaedcdcd17562558f0c172185d8
md-saheel <mdsaheel6666@gmail.com>
2023-11-09 17:48:49 +0530

    save data delivery options

---
+++ data/deliveryOptions.js
@@ -0,0 +1,17 @@
+export const deliveryOptions = [
+  {
+    id: '1',
+    deliveryDates: 7,
+    priceCents: 0,
+  },
+  {
+    id: '2',
+    deliveryDates: 3,
+    priceCents: 499,
+  },
+  {
+    id: '3',
+    deliveryDates: 1,
+    priceCents: 999,
+  },
+];
