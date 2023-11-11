0a1b5af80ab47ed4cba5183c5996e1af92b90780
md-saheel <mdsaheel6666@gmail.com>
2023-11-11 18:14:30 +0530

    updated date for delivery

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,6 +3,7 @@ import {
   removeFromCart,
   calculateCartQuantity,
   updateQuantity,
+  updateDeliveryOption,
 } from '../data/cart.js';
 import { products } from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
@@ -110,7 +111,10 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
     const isChecked = deliveryOption.id === cartItem.deliveryOptionId;
 
     html += `
-    <div class="delivery-option js-delivery-option">
+    <div class="delivery-option js-delivery-option"
+    data-product-id="${matchingProduct.id}" data-delivery-option-id="${
+      deliveryOption.id
+    }">
           <input
             type="radio" ${isChecked ? 'checked' : ''}
             class="delivery-option-input"
@@ -193,5 +197,12 @@ document.querySelectorAll('.js-save-link').forEach((link) => {
     quantityLabel.innerHTML = newQuantity;
 
     updateCartQuantity();
+  });
+});
+
+document.querySelectorAll('.js-delivery-option').forEach((element) => {
+  element.addEventListener('click', () => {
+    const { productId, deliveryOptionId } = element.dataset;
+    updateDeliveryOption(productId, deliveryOptionId);
   });
 });
